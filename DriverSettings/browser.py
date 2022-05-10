7e2e739abc39ae76115b241970525a3d7a9726f6
Sindromo <lis.mateusz@ymail.com>
2022-05-10 15:01:09 +0200

    Firefox as default

--- DriverSettings/browser.py
+++ DriverSettings/browser.py
@@ -6,8 +6,8 @@ from page_object.home_page import PuppyPage
 
 class browser():
     def browserSettings(self):
-        #self.deca = DesiredCapabilities().FIREFOX
-        self.deca = DesiredCapabilities().CHROME
+        self.deca = DesiredCapabilities().FIREFOX
+        #self.deca = DesiredCapabilities().CHROME
         self.deca["pageLoadStrategy"] = "normal"
         self.driver = webdriver.Firefox(executable_path=r'./Drivers/geckodriver.exe',desired_capabilities=self.deca)
         #self.driver = webdriver.Chrome(executable_path=r'./Drivers/chromedriver.exe', desired_capabilities=self.deca)
