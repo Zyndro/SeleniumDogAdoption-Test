21e607f3ce968ee69b484279b1799ef3dc259595
Sindromo <lis.mateusz@ymail.com>
2022-05-11 21:40:12 +0200

    test refactor

--- page_object/home_page.py
+++ page_object/home_page.py
@@ -6,7 +6,7 @@ from page_object.commodities_page import CommodityPage
 from page_object.overwiev_page import OverwievPage
 
 
-class PuppyPage():
+class PuppyHomePage():
     dognamelist = ["Brook", "Hanna", "Maggie Mae", "Ruby Sue", "Tipsy", "Spud", "Twinkie"]
     puppiesonpage = ["/html/body/div[1]/div[1]/div[3]/div[2]/div/div[2]/h3",
                      "/html/body/div[1]/div[1]/div[3]/div[3]/div/div[2]/h3",
@@ -19,7 +19,7 @@ class PuppyPage():
 
     def getPuppyLoc(self, name):
         dogname = str(name)
-        for x in PuppyPage.puppiesonpage:
+        for x in PuppyHomePage.puppiesonpage:
             puppy = self.driver.find_element(by=By.XPATH, value=x).text
             if str(name) == str(puppy):
                 size = len(x)
@@ -27,17 +27,17 @@ class PuppyPage():
                 mod_string = mod_string + "div[4]/form/input"
                 return mod_string
 
-        self.driver.find_element(by=By.XPATH, value=PuppyPage.txt_next_page).click()
+        self.driver.find_element(by=By.XPATH, value=PuppyHomePage.txt_next_page).click()
         # infinite recursion possible, dont care for now
-        return PuppyPage.getPuppyLoc(self, dogname)
+        return PuppyHomePage.getPuppyLoc(self, dogname)
 
-    def adddog(self,dogname=None , collar=False, chew=False, carrier=False, vet=False):
+    def addDogToOrder(self,dogname=None , collar=False, chew=False, carrier=False, vet=False):
         if dogname == None:
-            dog = random.choice(PuppyPage.dognamelist)
-            PuppyPage.dognamelist.remove(dog)
+            dog = random.choice(PuppyHomePage.dognamelist)
+            PuppyHomePage.dognamelist.remove(dog)
         else:
             dog = dogname
-        puppy = PuppyPage.getPuppyLoc(self, dog)
+        puppy = PuppyHomePage.getPuppyLoc(self, dog)
         self.driver.find_element(by=By.XPATH, value=puppy).click()
         self.driver.find_element(by=By.XPATH, value=OverwievPage.xp_confirm).click()
         CommodityPage.selectCommodities(self, collar, chew, carrier, vet)
