a177bb0518d4f1579beaaf156fbbb74d8c9b33ba
Sindromo <lis.mateusz@ymail.com>
2022-05-13 15:40:18 +0200

    test update

--- tests/test_spartantest_puppies_adoption_orders.py
+++ tests/test_spartantest_puppies_adoption_orders.py
@@ -1,5 +1,5 @@
-import unittest
 import random
+import unittest
 
 from DriverSettings.browsersetup import BrowserSetup
 from page_object.checkout_page import CheckoutPage
@@ -15,52 +15,53 @@ class AdoptionSiteTests(unittest.TestCase):
     # Adopt Brooke, add a Chewy Toy and a Travel Carrier, pay with Check
     def test_adoptDogBrook(self):
         PuppyHomePage.openPage(self)
-        PuppyHomePage.addDogToOrder(self, dogname="Brook", chew=True, carrier=True)
-        self.assertTrue(("Brook") in self.driver.page_source, True)
+        PuppyHomePage.addDogToOrder(self, dogname="Brook", chew=True, carrier=True, DogNumber=0)
+        CommodityPage.finalizeOrder(self)
         CommodityPage.clickComplete(self)
         CheckoutPage.fillData(self)
         CheckoutPage.paymentMethod(self, "Check")
         CheckoutPage.clickConfirm(self)
-        self.assertTrue(("Thank you for adopting a puppy!") in self.driver.page_source, True)
+        self.assertTrue(PuppyHomePage.purchaseSuccessCheck(self), True)
 
     # Adopt Sparky, add a Collar & Leash, pay with Credit Card
     def test_adoptDogSparky(self):
         PuppyHomePage.openPage(self)
-        PuppyHomePage.addDogToOrder(self, dogname="Sparky", collar=True)
-        self.assertTrue(("Sparky") in self.driver.page_source, True)
+        PuppyHomePage.addDogToOrder(self, dogname="Sparky", collar=True, DogNumber=0)
+        CommodityPage.finalizeOrder(self)
         CommodityPage.clickComplete(self)
         CheckoutPage.fillData(self)
         CheckoutPage.paymentMethod(self, "Credit card")
         CheckoutPage.clickConfirm(self)
-        self.assertTrue(("Thank you for adopting a puppy!") in self.driver.page_source, True)
+        self.assertTrue(PuppyHomePage.purchaseSuccessCheck(self), True)
 
     # Adopt 2 Random Dogs add a Collar & Leash to each, pay with Credit Card
     def test_adoptTwoRandomDogs(self):
         PuppyHomePage.openPage(self)
-        PuppyHomePage.addDogToOrder(self, collar=True)
+        PuppyHomePage.addDogToOrder(self, collar=True, DogNumber=0)
         CommodityPage.clickAnother(self)
-        PuppyHomePage.addDogToOrder(self, collar=True)
+        PuppyHomePage.addDogToOrder(self, collar=True, DogNumber=1)
+        CommodityPage.finalizeOrder(self)
         CommodityPage.clickComplete(self)
         CheckoutPage.fillData(self)
         CheckoutPage.paymentMethod(self, "Credit card")
         CheckoutPage.clickConfirm(self)
-        self.assertTrue(("Thank you for adopting a puppy!") in self.driver.page_source, True)
+        self.assertTrue(PuppyHomePage.purchaseSuccessCheck(self), True)
 
     # Adopt 2 Random Dogs add a 3 Random Accessories to 1, pay with Credit Card
     def test_adoptTwoRandomOneWithThreeAccessories(self):
         PuppyHomePage.openPage(self)
-        PuppyHomePage.addDogToOrder(self)
+        PuppyHomePage.addDogToOrder(self, DogNumber=0)
         CommodityPage.clickAnother(self)
         # randomly selecting 3 accessories
         accessories = [True, True, True, True]
         accessories[random.randint(0, 3)] = False
-        PuppyHomePage.addDogToOrder(self, collar=accessories[0], chew=accessories[1], carrier=accessories[2], vet=accessories[3])
+        PuppyHomePage.addDogToOrder(self, collar=accessories[0], chew=accessories[1], carrier=accessories[2], vet=accessories[3], DogNumber=1)
+        CommodityPage.finalizeOrder(self)
         CommodityPage.clickComplete(self)
         CheckoutPage.fillData(self)
         CheckoutPage.paymentMethod(self, "Credit card")
         CheckoutPage.clickConfirm(self)
-        self.assertTrue(("Thank you for adopting a puppy!") in self.driver.page_source, True)
-
+        self.assertTrue(PuppyHomePage.purchaseSuccessCheck(self), True)
 
     @classmethod
     def tearDownClass(self):
